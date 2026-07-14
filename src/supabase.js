59b9d42e0a8ad9e2f7c2127e18b78dd35e81b68c
AJAYISK <91030156+AJAYISK@users.noreply.github.com>
2026-07-14 06:25:35 +0100

    Change Supabase anonymous key
    Updated the Supabase anonymous key to a new value.

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://zpirnmzenrgsfqlbqets.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_T6xgx2PUecdStGCanJtQHQ_Rn0_O7S7'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwaXJubXplbnJnc2ZxbGJxZXRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5ODE2ODcsImV4cCI6MjA5OTU1NzY4N30._9SrzWVr_mqGcVHfb5Mu_uC-1wKoMZl9WzTYeKZ66Io'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
